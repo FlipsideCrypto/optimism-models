cbaabcba3318295e9c55fb699820529e247e04ff
Austin <93135983+austinFlipside@users.noreply.github.com>
2022-08-01 19:55:36 -0400

    Add/velo withdraws (#11)
    * stash
    
    * typo
    
    * docs

--- models/velodrome/silver__velodrome_locks.sql
+++ models/velodrome/silver__velodrome_locks.sql
@@ -16,9 +16,16 @@ WITH new_locks AS (
         origin_to_address,
         contract_address,
         event_index,
-        CONCAT('0x', SUBSTR(topics [1] :: STRING, 27, 40)) AS provider_address,
-        TO_TIMESTAMP(PUBLIC.udf_hex_to_int(topics [2] :: STRING)) AS unlock_date,
         regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
+        CONCAT('0x', SUBSTR(topics [1] :: STRING, 27, 40)) AS provider_address,
+        CASE
+            WHEN topics [0] :: STRING = '0xff04ccafc360e16b67d682d17bd9503c4c6b9a131f6be6325762dc9ffc7de624' THEN TO_TIMESTAMP(PUBLIC.udf_hex_to_int(topics [2] :: STRING))
+            WHEN topics [0] :: STRING = '0x02f25270a4d87bea75db541cdfe559334a275b4a233520ed6c0a2429667cca94' THEN TO_TIMESTAMP(
+                PUBLIC.udf_hex_to_int(
+                    segmented_data [2] :: STRING
+                )
+            )
+        END AS unlock_date,
         PUBLIC.udf_hex_to_int(
             segmented_data [0] :: STRING
         ) AS token_id,
@@ -30,15 +37,24 @@ WITH new_locks AS (
                 18
             )
         ) :: FLOAT AS velo_value,
-        PUBLIC.udf_hex_to_int(
-            segmented_data [2] :: STRING
-        ) AS deposit_type,
+        CASE
+            WHEN topics [0] :: STRING = '0xff04ccafc360e16b67d682d17bd9503c4c6b9a131f6be6325762dc9ffc7de624' THEN PUBLIC.udf_hex_to_int(
+                segmented_data [2] :: STRING
+            )
+        END AS deposit_type,
+        CASE
+            WHEN topics [0] :: STRING = '0xff04ccafc360e16b67d682d17bd9503c4c6b9a131f6be6325762dc9ffc7de624' THEN 'deposit'
+            WHEN topics [0] :: STRING = '0x02f25270a4d87bea75db541cdfe559334a275b4a233520ed6c0a2429667cca94' THEN 'withdraw'
+        END AS velo_action,
         _log_id,
         _inserted_timestamp
     FROM
         {{ ref('silver__logs') }}
     WHERE
-        topics [0] :: STRING = '0xff04ccafc360e16b67d682d17bd9503c4c6b9a131f6be6325762dc9ffc7de624' -- velo locks
+        topics [0] :: STRING IN (
+            '0xff04ccafc360e16b67d682d17bd9503c4c6b9a131f6be6325762dc9ffc7de624',
+            '0x02f25270a4d87bea75db541cdfe559334a275b4a233520ed6c0a2429667cca94'
+        ) -- velo locks / unlocks
         AND contract_address = '0x9c7305eb78a432ced5c4d14cac27e8ed569a2e26'
         AND tx_status = 'SUCCESS'
         AND event_removed = 'false'
@@ -80,6 +96,7 @@ SELECT
     event_index,
     provider_address,
     unlock_date,
+    velo_action,
     token_id :: NUMBER AS token_id,
     velo_value AS velo_amount,
     ROUND(
