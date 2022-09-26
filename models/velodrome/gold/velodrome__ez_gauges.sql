9df206bc786574f2d6ed0286bbf5129cdf2c4858
Austin <austin@flipsidecrypto.com>
2022-09-26 13:03:00 -0400

    gauges and votes

---
+++ models/velodrome/gold/velodrome__ez_gauges.sql
@@ -0,0 +1,29 @@
+{{ config(
+    materialized = 'view',
+    persist_docs ={ "relation": true,
+    "columns": true },
+    tags = ['velodrome']
+) }}
+
+SELECT
+    block_timestamp,
+    block_number,
+    tx_hash,
+    event_index,
+    contract_address,
+    gauge_address,
+    external_bribe_address,
+    internal_bribe_address,
+    creator_address,
+    A.pool_address AS pool_address,
+    pool_name,
+    pool_type,
+    token0_symbol,
+    token1_symbol,
+    token0_address,
+    token1_address
+FROM
+    {{ ref('silver__velodrome_gauges') }} A
+    LEFT JOIN {{ ref('silver__velodrome_pools') }}
+    b
+    ON A.pool_address = b.pool_address
--- models/velodrome/gold/velodrome__ez_votes.sql
+++ models/velodrome/gold/velodrome__ez_votes.sql
@@ -6,20 +6,50 @@
 ) }}
 
 SELECT
-    block_number,
-    block_timestamp,
-    tx_hash,
-    origin_function_signature,
-    origin_from_address,
-    origin_to_address,
-    gauge_address,
-    external_bribe_address,
-    internal_bribe_address,
-    pool_address,
-    pool_name,
+    base.block_number AS block_number,
+    base.block_timestamp AS block_timestamp,
+    base.tx_hash AS tx_hash,
+    base.origin_function_signature AS origin_function_signature,
+    base.origin_from_address AS origin_from_address,
+    base.origin_to_address AS origin_to_address,
+    COALESCE(
+        g1.gauge_address,
+        g0.gauge_address
+    ) AS gauge_address,
+    COALESCE(
+        g1.external_bribe_address,
+        g0.external_bribe_address
+    ) AS external_bribe_address,
+    COALESCE(
+        g1.internal_bribe_address,
+        g0.internal_bribe_address
+    ) AS internal_bribe_address,
+    COALESCE(
+        g1.pool_address,
+        g0.pool_address
+    ) AS pool_address,
+    COALESCE(
+        g1.pool_name,
+        g0.pool_name
+    ) AS pool_name,
     from_address,
     token_id,
     vote_amount,
     vote_action
 FROM
     {{ ref('silver__velodrome_votes') }}
+    base
+    LEFT JOIN {{ ref('velodrome__ez_gauges') }}
+    g1
+    ON LOWER(
+        base.contract_address
+    ) = LOWER(
+        g1.external_bribe_address
+    )
+    LEFT JOIN {{ ref('velodrome__ez_gauges') }}
+    g0
+    ON LOWER(
+        base.contract_address
+    ) = LOWER(
+        g0.internal_bribe_address
+    )
