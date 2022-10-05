62644874a6a2a9e9b023853c24ec420c5c0ddbf6
Jessica Huhnke <91915469+jhuhnke@users.noreply.github.com>
2022-10-04 20:26:36 -0500

    add value columns in delegate table (#34)

--- models/silver/silver__delegations.sql
+++ models/silver/silver__delegations.sql
@@ -1,13 +1,13 @@
 {{ config(
     materialized = 'incremental',
-    unique_key = "tx_hash",
+    unique_key = "_log_id",
     cluster_by = ['block_timestamp::DATE']
 ) }}
 
 SELECT 
-    block_number, 
-    block_timestamp,
-    tx_hash,
+    t.block_number, 
+    t.block_timestamp,
+    t.tx_hash,
     status, 
     from_address AS delegator,
     CASE WHEN tx_json:receipt:logs[0]:decoded:inputs:fromDelegate :: STRING = '0x0000000000000000000000000000000000000000' AND tx_json :receipt :logs[0] :decoded :inputs :toDelegate :: STRING <> from_address THEN 
@@ -20,7 +20,7 @@ SELECT
         'Re-Delegation' 
     END AS delegation_type, 
     CASE WHEN delegation_type = 'Re-Delegation' THEN 
-        CONCAT('0x', SUBSTR(tx_json :receipt :logs[0] :topics[1] :: STRING, 27, 40)) 
+        l.event_inputs :delegate :: STRING 
     ELSE 
         tx_json :receipt :logs[0] :decoded :inputs :toDelegate :: STRING
     END AS to_delegate, 
@@ -29,15 +29,31 @@ SELECT
     ELSE 
         NULL 
     END AS from_delegate, 
-    _inserted_timestamp
-  FROM 
-    {{ ref('silver__transactions') }}
-  WHERE 
-    origin_function_signature = '0x5c19a95c'
+    l.event_inputs :newBalance :: NUMBER AS raw_new_balance, 
+    l.event_inputs :previousBalance :: NUMBER AS raw_previous_balance,
+    t._inserted_timestamp, 
+    l._log_id
+FROM 
+    {{ ref('silver__transactions') }} t
+
+LEFT OUTER JOIN {{ ref('silver__logs')}} l 
+ON t.tx_hash = l.tx_hash
+
+WHERE 
+    t.origin_function_signature = '0x5c19a95c'
     AND to_address = '0x4200000000000000000000000000000000000042'
+    AND event_name = 'DelegateVotesChanged'
 
 {% if is_incremental() %}
-AND _inserted_timestamp >= (
+AND t._inserted_timestamp >= (
+    SELECT
+        MAX(
+            _inserted_timestamp
+        )
+    FROM
+        {{ this }}
+)
+AND l._inserted_timestamp >= (
     SELECT
         MAX(
             _inserted_timestamp
