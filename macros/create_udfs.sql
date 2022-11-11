d2c84765a102f37abdbe048ff9811ac33bc3d606
drethereum <trevor.wenokur@gmail.com>
2022-11-11 10:08:39 -0700

    readjusted spacing in macros

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -1,13 +1,13 @@
 {% macro create_udfs() %}
     {% if var("UPDATE_UDFS_AND_SPS") %}
-            {% set sql %}
-            CREATE schema if NOT EXISTS silver;
-        {{ create_js_hex_to_int() }};
-        {{ create_udf_hex_to_int(
-                schema = "public"
-            ) }}
+        {% set sql %}
+        CREATE schema if NOT EXISTS silver;
+    {{ create_js_hex_to_int() }};
+    {{ create_udf_hex_to_int(
+            schema = "public"
+        ) }}
 
-            {% endset %}
-            {% do run_query(sql) %}
+        {% endset %}
+        {% do run_query(sql) %}
     {% endif %}
 {% endmacro %}
