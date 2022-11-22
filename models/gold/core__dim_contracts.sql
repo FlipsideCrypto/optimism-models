f2ce182cfbfb18d516018f478feb4c64adfccb67
Austin <austin@flipsidecrypto.com>
2022-11-22 10:36:04 -0500

    stash

--- models/gold/core__dim_contracts.sql
+++ models/gold/core__dim_contracts.sql
@@ -5,8 +5,10 @@
 ) }}
 
 SELECT
-    LOWER(address) :: STRING AS address,
-    symbol :: STRING AS symbol,
-    decimals :: INTEGER AS decimals
+    contract_address,
+    token_symbol AS symbol,
+    token_name AS NAME,
+    token_decimals AS decimals,
+    contract_address AS address
 FROM
-    {{ ref('silver__contracts_backfill') }}
+    {{ ref('silver__contracts') }}
