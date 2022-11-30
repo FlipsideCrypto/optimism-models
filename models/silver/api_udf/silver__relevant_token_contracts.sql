5df6f31b724eac3d9da45a902c4de4242e5a14e1
Austin <93135983+austinFlipside@users.noreply.github.com>
2022-11-30 14:03:51 -0500

    all contracts (#53)

--- models/silver/api_udf/silver__relevant_token_contracts.sql
+++ models/silver/api_udf/silver__relevant_token_contracts.sql
@@ -10,8 +10,6 @@ SELECT
     MIN(block_number) AS created_block
 FROM
     {{ ref('silver__logs') }}
-WHERE
-    topics [0] :: STRING = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
 GROUP BY
     1,
     2
