2a11817c7d06d7b55896d42fe6551aef25262aae
drethereum <trevor.wenokur@gmail.com>
2023-01-18 11:14:01 -0700

    added univ3 to dex_swaps and added not null event name test

--- models/gold/dex/core__ez_dex_swaps.sql
+++ models/gold/dex/core__ez_dex_swaps.sql
@@ -15,7 +15,10 @@ WITH velodrome_swaps AS (
     origin_to_address,
     contract_address,
     pool_name,
-    event_name,
+    CASE 
+      WHEN event_name IS NULL THEN 'Swap' 
+      ELSE event_name 
+    END AS event_name,
     amount_in,
     amount_in_usd,
     amount_out,
@@ -42,7 +45,10 @@ sushi_swaps AS (
     origin_to_address,
     contract_address,
     pool_name,
-    event_name,
+    CASE 
+      WHEN event_name IS NULL THEN 'Swap' 
+      ELSE event_name 
+    END AS event_name,
     amount_in,
     amount_in_usd,
     amount_out,
@@ -85,6 +91,57 @@ synthetix_swaps AS (
     _log_id
   FROM
     {{ ref('silver_dex__synthetix_swaps') }}
+),
+univ3_swaps AS (
+  SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    origin_function_signature,
+    origin_from_address,
+    origin_to_address,
+    pool_address AS contract_address,
+    pool_name,
+    'Swap' AS event_name,
+    CASE
+      WHEN amount0_unadj > 0 THEN ABS(amount0_adjusted)
+      ELSE ABS(amount1_adjusted)
+    END AS amount_in,
+    CASE
+      WHEN amount0_unadj > 0 THEN ABS(amount0_usd)
+      ELSE ABS(amount1_usd)
+    END AS amount_in_usd,
+    CASE
+      WHEN amount0_unadj < 0 THEN ABS(amount0_adjusted)
+      ELSE ABS(amount1_adjusted)
+    END AS amount_out,
+    CASE
+      WHEN amount0_unadj < 0 THEN ABS(amount0_usd)
+      ELSE ABS(amount1_usd)
+    END AS amount_out_usd,
+    sender,
+    recipient AS tx_to,
+    event_index,
+    'uniswap-v3' AS platform,
+    CASE
+      WHEN amount0_unadj > 0 THEN token0_address
+      ELSE token1_address
+    END AS token_in,
+    CASE
+      WHEN amount0_unadj < 0 THEN token0_address
+      ELSE token1_address
+    END AS token_out,
+    CASE
+      WHEN amount0_unadj > 0 THEN token0_symbol
+      ELSE token1_symbol
+    END AS symbol_in,
+    CASE
+      WHEN amount0_unadj < 0 THEN token0_symbol
+      ELSE token1_symbol
+    END AS symbol_out,
+    _log_id
+  FROM
+    {{ ref('silver_dex__univ3_swaps') }}
 )
 
 SELECT
@@ -96,10 +153,7 @@ SELECT
   origin_to_address,
   contract_address,
   pool_name,
-  CASE 
-    WHEN event_name IS NULL THEN 'Swap' 
-    ELSE event_name 
-  END AS event_name,
+  event_name,
   amount_in,
   amount_in_usd,
   amount_out,
@@ -125,10 +179,7 @@ SELECT
   origin_to_address,
   contract_address,
   pool_name,
-  CASE 
-    WHEN event_name IS NULL THEN 'Swap' 
-    ELSE event_name 
-  END AS event_name,
+  event_name,
   amount_in,
   amount_in_usd,
   amount_out,
@@ -170,3 +221,29 @@ SELECT
   _log_id
 FROM
   synthetix_swaps
+UNION ALL
+SELECT
+  block_number,
+  block_timestamp,
+  tx_hash,
+  origin_function_signature,
+  origin_from_address,
+  origin_to_address,
+  contract_address,
+  pool_name,
+  event_name,
+  amount_in,
+  amount_in_usd,
+  amount_out,
+  amount_out_usd,
+  sender,
+  tx_to,
+  event_index,
+  platform,
+  token_in,
+  token_out,
+  symbol_in,
+  symbol_out,
+  _log_id
+FROM
+  univ3_swaps
