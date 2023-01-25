6ba7b96870d167284a6a4d96e9792d8294bd3959
drethereum <trevor.wenokur@gmail.com>
2023-01-25 13:25:28 -0700

    snowflake tagging for ez_dex_swaps

--- models/gold/dex/core__ez_dex_swaps.sql
+++ models/gold/dex/core__ez_dex_swaps.sql
@@ -1,7 +1,15 @@
 {{ config(
     materialized = 'view',
     persist_docs ={ "relation": true,
-    "columns": true }
+    "columns": true },
+    meta={
+        'database_tags':{
+            'table': {
+                'PROTOCOL': 'SUSHI, UNISWAP, CURVE, SYNTHETIX, VELODROME',
+                'PURPOSE': 'DEX, SWAPS'
+            }
+        }
+    }
 ) }}
 
 WITH velodrome_swaps AS (
