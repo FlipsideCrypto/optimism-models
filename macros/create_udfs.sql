2a7a7611993a3699a04f12dcb10a868a0c43690e
drethereum <trevor.wenokur@gmail.com>
2023-06-06 11:36:46 -0600

    create_udfs

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -2,7 +2,7 @@
     {% if var("UPDATE_UDFS_AND_SPS") %}
         {% set sql %}
         CREATE schema if NOT EXISTS silver;
-{{ create_udf_keccak(
+        {{ create_udf_keccak(
             schema = 'silver'
         ) }}
         {{ create_udf_simple_event_names(
@@ -11,9 +11,6 @@
 
         {% endset %}
         {% do run_query(sql) %}
-        {% set name %}
         {{- fsc_utils.create_udfs() -}}
-        {% endset %}
-        {% do run_query(sql) %}
     {% endif %}
 {% endmacro %}
