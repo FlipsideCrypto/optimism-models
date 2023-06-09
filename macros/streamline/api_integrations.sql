135ec945d90c539e0d6aea9738c6c8a16e1078cc
shah <info@shahnewazkhan.ca>
2023-06-08 19:49:54 -0700

    added api integrations, udft and streamline udfs

--- macros/streamline/api_integrations.sql
+++ macros/streamline/api_integrations.sql
@@ -10,14 +10,14 @@
     {% elif target.name == "dev" %}
         {% set sql %}
         CREATE api integration IF NOT EXISTS aws_optimism_api_dev api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::490041342817:role/snowflake-api-optimism' api_allowed_prefixes = (
-            '<DEV_URL_PLACEHOLDER>'
+            'https://4sovbxzgsf.execute-api.us-east-1.amazonaws.com/dev/'
         ) enabled = TRUE;
         {% endset %}
         {% do run_query(sql) %}
     {% elif target.name == "sbx" %}
         {% set sql %}
         CREATE api integration IF NOT EXISTS aws_optimism_api_sbx_shah api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::579011195466:role/snowflake-api-optimism' api_allowed_prefixes = (
-            'SBX_URL_PLACEHOLDER'
+            'https://3ifufl19z4.execute-api.us-east-1.amazonaws.com/sbx/'
         ) enabled = TRUE;
         {% endset %}
         {% do run_query(sql) %}
