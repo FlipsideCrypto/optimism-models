83d177b173d8d1878d57c69cd78f629691c98084
shah <info@shahnewazkhan.ca>
2023-06-12 21:19:47 -0700

    added create_udf macros

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -9,6 +9,13 @@
             schema = 'silver'
         ) }}
 
+        {{ create_udtf_get_base_table(
+            schema = "streamline"
+        ) }}
+        {{ create_udf_get_chainhead() }}
+        {{ create_udf_bulk_json_rpc() }}
+        {{ create_udf_bulk_decode_logs() }}
+
         {% endset %}
         {% do run_query(sql) %}
         {{- fsc_utils.create_udfs() -}}
