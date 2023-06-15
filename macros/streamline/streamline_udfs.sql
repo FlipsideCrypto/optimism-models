ea32c54c301a393bb0b907da80ca3d763219f03f
shah <info@shahnewazkhan.ca>
2023-06-15 12:15:32 -0700

    removed sbx udf definitions

--- macros/streamline/streamline_udfs.sql
+++ macros/streamline/streamline_udfs.sql
@@ -3,7 +3,7 @@
     OR REPLACE EXTERNAL FUNCTION streamline.udf_get_chainhead() returns variant api_integration = aws_avalanche_api AS {% if target.name == "prod" %}
         'https://s7qxto6wkd.execute-api.us-east-1.amazonaws.com/prod/get_chainhead'
     {% else %}
-        'https://3ifufl19z4.execute-api.us-east-1.amazonaws.com/sbx/get_chainhead'
+        'https://4sovbxzgsf.execute-api.us-east-1.amazonaws.com/dev/get_chainhead'
     {%- endif %};
 {% endmacro %}
 
@@ -14,7 +14,7 @@
     ) returns text api_integration = aws_terra_api AS {% if target.name == "prod" %}
         'https://s7qxto6wkd.execute-api.us-east-1.amazonaws.com/prod/udf_bulk_json_rpc'
     {% else %}
-        'https://3ifufl19z4.execute-api.us-east-1.amazonaws.com/sbx/udf_bulk_json_rpc'
+        'https://4sovbxzgsf.execute-api.us-east-1.amazonaws.com/dev/udf_bulk_json_rpc'
     {%- endif %};
 {% endmacro %}
 
@@ -25,7 +25,7 @@
     ) returns ARRAY api_integration = aws_arbitrum_api AS {% if target.name == "prod" %}
         'https://s7qxto6wkd.execute-api.us-east-1.amazonaws.com/prod/bulk_decode_logs'
     {% else %}
-        'https://3ifufl19z4.execute-api.us-east-1.amazonaws.com/sbx/bulk_decode_logs'
+        'https://4sovbxzgsf.execute-api.us-east-1.amazonaws.com/dev/bulk_decode_logs'
     {%- endif %};
 {% endmacro %}
 
@@ -33,9 +33,9 @@
     CREATE
     OR REPLACE EXTERNAL FUNCTION streamline.udf_bulk_get_traces(
         json variant
-    ) returns text api_integration = aws_arbitrum_api AS {% if target.name == "prod" %}
+    ) returns text api_integration = aws_optimism_api AS {% if target.name == "prod" %}
         'https://s7qxto6wkd.execute-api.us-east-1.amazonaws.com/prod/udf_bulk_get_traces'
     {% else %}
-        'ttps://4sovbxzgsf.execute-api.us-east-1.amazonaws.com/dev/udf_bulk_get_traces'
+        'https://4sovbxzgsf.execute-api.us-east-1.amazonaws.com/dev/udf_bulk_get_traces'
     {%- endif %};
 {% endmacro %}
