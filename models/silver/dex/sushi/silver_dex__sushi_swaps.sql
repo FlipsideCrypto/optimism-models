caeaaf3b7529bae1d145a89402d8ce7185b43045
mattromano <mattromano@flipsidecrypto.com>
2023-06-27 09:21:07 -0700

    update sushi dex swaps

--- models/silver/dex/sushi/silver_dex__sushi_swaps.sql
+++ models/silver/dex/sushi/silver_dex__sushi_swaps.sql
@@ -25,12 +25,12 @@ swaps_base AS (
         contract_address,
         regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
         TRY_TO_NUMBER(
-            ethereum.public.udf_hex_to_int(
+            utils.udf_hex_to_int(
                 segmented_data [0] :: STRING
             )
         ) AS amountIn,
         TRY_TO_NUMBER(
-            ethereum.public.udf_hex_to_int(
+            utils.udf_hex_to_int(
                 segmented_data [1] :: STRING
             )
         ) AS amountOut,
