bc6612303c8fde71ab4eb8bacc8d8e9be905e584
Shah Newaz Khan <shahnewazk@gmail.com>
2023-07-12 09:09:43 -0700

    added exploded key and method udf params for non batch traces (#118)
    * added exploded key and method udf params for non batch traces
    
    * increased producer_batch_size to double worker to reach 1000 RPS set by traces indexer from QN
    
    * increased gha frequency to 6 hours
    
    * reduced gha frequency to 6
    
    ---------
    
    Co-authored-by: shah <info@shahnewazkhan.ca>

--- models/silver/streamline/core/history/streamline__debug_traceBlockByNumber_history.sql
+++ models/silver/streamline/core/history/streamline__debug_traceBlockByNumber_history.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_bulk_get_traces(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'debug_traceBlockByNumber', 'sql_limit', {{var('sql_limit','12960000')}}, 'producer_batch_size', {{var('producer_batch_size','460000')}}, 'worker_batch_size', {{var('worker_batch_size','230000')}}, 'batch_call_limit', {{var('batch_call_limit','1')}}))",
+        func = "{{this.schema}}.udf_bulk_get_traces(object_construct('sql_source', '{{this.identifier}}','exploded_key','[\"result\"]', 'method', 'debug_traceBlockByNumber', 'external_table', 'debug_traceBlockByNumber', 'sql_limit', {{var('sql_limit','12960000')}}, 'producer_batch_size', {{var('producer_batch_size','920000')}}, 'worker_batch_size', {{var('worker_batch_size','230000')}}, 'batch_call_limit', {{var('batch_call_limit','1')}}))",
         target = "{{this.schema}}.{{this.identifier}}"
     )
 ) }}
