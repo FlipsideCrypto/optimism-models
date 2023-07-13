ceea7b6ff3156ecfd38461a7adc5132c0cc432d8
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2023-07-13 13:22:31 -0400

    Speed up confirm blocks backfill (#119)

--- models/silver/streamline/core/realtime/streamline__confirm_blocks_realtime.sql
+++ models/silver/streamline/core/realtime/streamline__confirm_blocks_realtime.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_bulk_json_rpc(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'confirm_blocks', 'sql_limit', {{var('sql_limit','35000')}}, 'producer_batch_size', {{var('producer_batch_size','35000')}}, 'worker_batch_size', {{var('worker_batch_size','8750')}}, 'batch_call_limit', {{var('batch_call_limit','10')}}))",
+        func = "{{this.schema}}.udf_bulk_json_rpc(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'confirm_blocks', 'sql_limit', {{var('sql_limit','1000000')}}, 'producer_batch_size', {{var('producer_batch_size','100000')}}, 'worker_batch_size', {{var('worker_batch_size','1000')}}, 'batch_call_limit', {{var('batch_call_limit','10')}}))",
         target = "{{this.schema}}.{{this.identifier}}"
     )
 ) }}
