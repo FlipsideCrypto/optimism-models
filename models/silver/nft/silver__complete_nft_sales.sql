1ea173e5d89546e711ac8afec3a131f553f550b1
Sam <110511194+sam-xyz@users.noreply.github.com>
2023-07-27 09:29:03 +0800

    An 3599/add quix and seaport (#139)
    * add quix, and quix's seaport
    
    * add quix and quix's seaport
    
    * seaport
    
    * adjust dates
    
    * add seaport
    
    * date change
    
    * .
    
    * tags format
    
    ---------
    
    Co-authored-by: drethereum <trevor.wenokur@gmail.com>

---
+++ models/silver/nft/silver__complete_nft_sales.sql
@@ -0,0 +1,422 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = 'nft_log_id',
+    cluster_by = ['block_timestamp::DATE']
+) }}
+
+WITH nft_base_models AS (
+
+    SELECT
+        block_number,
+        block_timestamp,
+        tx_hash,
+        event_type,
+        platform_address,
+        platform_name,
+        platform_exchange_version,
+        seller_address,
+        buyer_address,
+        nft_address,
+        erc1155_value,
+        tokenId,
+        currency_address,
+        total_price_raw,
+        total_fees_raw,
+        platform_fee_raw,
+        creator_fee_raw,
+        tx_fee,
+        origin_from_address,
+        origin_to_address,
+        origin_function_signature,
+        input_data,
+        nft_log_id,
+        _log_id,
+        _inserted_timestamp
+    FROM
+        {{ ref('silver__quix_sales') }}
+
+{% if is_incremental() %}
+WHERE
+    _inserted_timestamp >= (
+        SELECT
+            MAX(
+                _inserted_timestamp
+            ) :: DATE - 1
+        FROM
+            {{ this }}
+    )
+{% endif %}
+UNION ALL
+SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    event_type,
+    platform_address,
+    platform_name,
+    platform_exchange_version,
+    seller_address,
+    buyer_address,
+    nft_address,
+    erc1155_value,
+    tokenId,
+    currency_address,
+    total_price_raw,
+    total_fees_raw,
+    platform_fee_raw,
+    creator_fee_raw,
+    tx_fee,
+    origin_from_address,
+    origin_to_address,
+    origin_function_signature,
+    input_data,
+    nft_log_id,
+    _log_id,
+    _inserted_timestamp
+FROM
+    {{ ref('silver__quix_seaport_sales') }}
+
+{% if is_incremental() %}
+WHERE
+    _inserted_timestamp >= (
+        SELECT
+            MAX(
+                _inserted_timestamp
+            ) :: DATE - 1
+        FROM
+            {{ this }}
+    )
+{% endif %}
+UNION ALL
+SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    event_type,
+    platform_address,
+    platform_name,
+    platform_exchange_version,
+    seller_address,
+    buyer_address,
+    nft_address,
+    erc1155_value,
+    tokenId,
+    currency_address,
+    total_price_raw,
+    total_fees_raw,
+    platform_fee_raw,
+    creator_fee_raw,
+    tx_fee,
+    origin_from_address,
+    origin_to_address,
+    origin_function_signature,
+    input_data,
+    nft_log_id,
+    _log_id,
+    _inserted_timestamp
+FROM
+    {{ ref('silver__seaport_1_1_sales') }}
+
+{% if is_incremental() %}
+WHERE
+    _inserted_timestamp >= (
+        SELECT
+            MAX(
+                _inserted_timestamp
+            ) :: DATE - 1
+        FROM
+            {{ this }}
+    )
+{% endif %}
+UNION ALL
+SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    event_type,
+    platform_address,
+    platform_name,
+    platform_exchange_version,
+    seller_address,
+    buyer_address,
+    nft_address,
+    erc1155_value,
+    tokenId,
+    currency_address,
+    total_price_raw,
+    total_fees_raw,
+    platform_fee_raw,
+    creator_fee_raw,
+    tx_fee,
+    origin_from_address,
+    origin_to_address,
+    origin_function_signature,
+    input_data,
+    nft_log_id,
+    _log_id,
+    _inserted_timestamp
+FROM
+    {{ ref('silver__seaport_1_4_sales') }}
+
+{% if is_incremental() %}
+WHERE
+    _inserted_timestamp >= (
+        SELECT
+            MAX(
+                _inserted_timestamp
+            ) :: DATE - 1
+        FROM
+            {{ this }}
+    )
+{% endif %}
+UNION ALL
+SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    event_type,
+    platform_address,
+    platform_name,
+    platform_exchange_version,
+    seller_address,
+    buyer_address,
+    nft_address,
+    erc1155_value,
+    tokenId,
+    currency_address,
+    total_price_raw,
+    total_fees_raw,
+    platform_fee_raw,
+    creator_fee_raw,
+    tx_fee,
+    origin_from_address,
+    origin_to_address,
+    origin_function_signature,
+    input_data,
+    nft_log_id,
+    _log_id,
+    _inserted_timestamp
+FROM
+    {{ ref('silver__seaport_1_5_sales') }}
+
+{% if is_incremental() %}
+WHERE
+    _inserted_timestamp >= (
+        SELECT
+            MAX(
+                _inserted_timestamp
+            ) :: DATE - 1
+        FROM
+            {{ this }}
+    )
+{% endif %}
+),
+prices_raw AS (
+    SELECT
+        HOUR,
+        symbol,
+        token_address,
+        decimals,
+        price AS hourly_prices
+    FROM
+        {{ ref('core__fact_hourly_token_prices') }}
+    WHERE
+        token_address IN (
+            SELECT
+                DISTINCT currency_address
+            FROM
+                nft_base_models
+        )
+        AND HOUR :: DATE IN (
+            SELECT
+                DISTINCT block_timestamp :: DATE
+            FROM
+                nft_base_models
+        )
+
+{% if is_incremental() %}
+AND HOUR >= (
+    SELECT
+        MAX(_inserted_timestamp) :: DATE - 2
+    FROM
+        {{ this }}
+)
+{% endif %}
+),
+all_prices AS (
+    SELECT
+        HOUR,
+        symbol,
+        token_address,
+        decimals,
+        hourly_prices
+    FROM
+        prices_raw
+    UNION ALL
+    SELECT
+        HOUR,
+        'ETH' AS symbol,
+        'ETH' AS token_address,
+        decimals,
+        hourly_prices
+    FROM
+        prices_raw
+    WHERE
+        token_address = '0x4200000000000000000000000000000000000006'
+),
+eth_price AS (
+    SELECT
+        HOUR,
+        hourly_prices AS eth_price_hourly
+    FROM
+        prices_raw
+    WHERE
+        token_address = '0x4200000000000000000000000000000000000006'
+),
+nft_labels AS (
+    SELECT
+        contract_address,
+        token_name
+    FROM
+        {{ ref('silver__contracts') }}
+    WHERE
+        contract_address IS NOT NULL
+        AND token_name IS NOT NULL
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp) :: DATE - 2
+    FROM
+        {{ this }}
+)
+{% endif %}
+
+qualify ROW_NUMBER() over (
+    PARTITION BY contract_address
+    ORDER BY
+        _inserted_timestamp DESC
+) = 1
+),
+final_base AS (
+    SELECT
+        block_number,
+        block_timestamp,
+        tx_hash,
+        event_type,
+        platform_address,
+        CASE
+            WHEN origin_to_address IN (
+                '0xbbbbbbbe843515689f3182b748b5671665541e58'
+            ) THEN 'bluesweep'
+            ELSE platform_name
+        END AS platform_name,
+        platform_exchange_version,
+        NULL AS aggregator_name,
+        seller_address,
+        buyer_address,
+        nft_address,
+        l.token_name AS project_name,
+        erc1155_value,
+        tokenId,
+        p.symbol AS currency_symbol,
+        currency_address,
+        total_price_raw,
+        total_fees_raw,
+        platform_fee_raw,
+        creator_fee_raw,
+        CASE
+            WHEN currency_address IN (
+                'ETH',
+                '0x4200000000000000000000000000000000000042',
+                '0x4200000000000000000000000000000000000006'
+            ) THEN total_price_raw / pow(
+                10,
+                18
+            )
+            ELSE COALESCE (total_price_raw / pow(10, decimals), total_price_raw)
+        END AS price,
+        IFF(
+            decimals IS NULL,
+            0,
+            price * hourly_prices
+        ) AS price_usd,
+        CASE
+            WHEN currency_address IN (
+                'ETH',
+                '0x4200000000000000000000000000000000000042',
+                '0x4200000000000000000000000000000000000006'
+            ) THEN total_fees_raw / pow(
+                10,
+                18
+            )
+            ELSE COALESCE (total_fees_raw / pow(10, decimals), total_fees_raw)
+        END AS total_fees,
+        IFF(
+            decimals IS NULL,
+            0,
+            total_fees * hourly_prices
+        ) AS total_fees_usd,
+        CASE
+            WHEN currency_address IN (
+                'ETH',
+                '0x4200000000000000000000000000000000000042',
+                '0x4200000000000000000000000000000000000006'
+            ) THEN platform_fee_raw / pow(
+                10,
+                18
+            )
+            ELSE COALESCE (platform_fee_raw / pow(10, decimals), platform_fee_raw)
+        END AS platform_fee,
+        IFF(
+            decimals IS NULL,
+            0,
+            platform_fee * hourly_prices
+        ) AS platform_fee_usd,
+        CASE
+            WHEN currency_address IN (
+                'ETH',
+                '0x4200000000000000000000000000000000000042',
+                '0x4200000000000000000000000000000000000006'
+            ) THEN creator_fee_raw / pow(
+                10,
+                18
+            )
+            ELSE COALESCE (creator_fee_raw / pow(10, decimals), creator_fee_raw)
+        END AS creator_fee,
+        IFF(
+            decimals IS NULL,
+            0,
+            creator_fee * hourly_prices
+        ) AS creator_fee_usd,
+        tx_fee,
+        tx_fee * eth_price_hourly AS tx_fee_usd,
+        origin_from_address,
+        origin_to_address,
+        origin_function_signature,
+        nft_log_id,
+        input_data,
+        _log_id,
+        _inserted_timestamp
+    FROM
+        nft_base_models b
+        LEFT JOIN nft_labels l
+        ON b.nft_address = l.contract_address
+        LEFT JOIN all_prices p
+        ON DATE_TRUNC(
+            'hour',
+            b.block_timestamp
+        ) = p.hour
+        AND b.currency_address = p.token_address
+        LEFT JOIN eth_price e
+        ON DATE_TRUNC(
+            'hour',
+            b.block_timestamp
+        ) = e.hour
+)
+SELECT
+    *
+FROM
+    final_base qualify(ROW_NUMBER() over(PARTITION BY nft_log_id
+ORDER BY
+    _inserted_timestamp DESC)) = 1
