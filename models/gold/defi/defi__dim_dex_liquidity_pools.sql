ca6ad84570b3ad5ca75b5056a6cc4944243539e1
drethereum <trevor.wenokur@gmail.com>
2023-08-02 12:03:18 -0600

    docs

--- models/gold/defi/defi__dim_dex_liquidity_pools.sql
+++ models/gold/defi/defi__dim_dex_liquidity_pools.sql
@@ -5,7 +5,7 @@
     meta={
     'database_tags':{
         'table': {
-            'PROTOCOL': 'BEETHOVENX, CURVE, DODO, FRAXSWAP, KYBERSWAP, SYNTHETIX, SUSHISWAP, UNISWAP, VELODROME, WOOFI',
+            'PROTOCOL': 'BEETHOVENX, CURVE, DODO, FRAXSWAP, KYBERSWAP, SUSHISWAP, UNISWAP, VELODROME',
             'PURPOSE': 'DEX, LIQUIDITY, POOLS, LP, SWAPS',
             }
         }
