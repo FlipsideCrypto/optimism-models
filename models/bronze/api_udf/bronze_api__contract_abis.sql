64427f7dbd7912bb3bfd9c92fc91e48177ffda06
Austin <austin@flipsidecrypto.com>
2023-08-07 18:10:27 -0400

    remove logic

--- models/bronze/api_udf/bronze_api__contract_abis.sql
+++ models/bronze/api_udf/bronze_api__contract_abis.sql
@@ -33,49 +33,7 @@ WHERE
     abi_data :data :result :: STRING <> 'Max rate limit reached'
 {% endif %}
 LIMIT
-    50
-), all_contracts AS (
-    SELECT
-        contract_address
-    FROM
-        base
-    UNION
-    SELECT
-        contract_address
-    FROM
-        (
-            SELECT
-                contract_address,
-                MAX(block_number) AS block_number,
-                COUNT(*) AS events
-            FROM
-                {{ ref("silver__logs") }}
-                l
-                LEFT JOIN {{ source(
-                    'optimism_silver',
-                    'verified_abis'
-                ) }}
-                -- this has to be a source or else we get a circular dependency
-                v USING (contract_address)
-            WHERE
-                l.block_timestamp >= CURRENT_DATE - INTERVAL '30 days' -- recent activity
-                AND v.contract_address IS NULL -- no verified abi
-                AND l.contract_address NOT IN (
-                    SELECT
-                        contract_address
-                    FROM
-                        {{ this }}
-                    WHERE
-                        _inserted_timestamp >= CURRENT_DATE - INTERVAL '30 days' -- this wont let us retry the same contract within 30 days
-                        AND abi_data :data :result :: STRING <> 'Max rate limit reached' -- exclude contracts that we were rate limited on
-                )
-            GROUP BY
-                contract_address
-            ORDER BY
-                events DESC
-            LIMIT
-                50
-        )
+    300
 ), row_nos AS (
     SELECT
         contract_address,
@@ -83,21 +41,29 @@ LIMIT
             ORDER BY
                 contract_address
         ) AS row_no,
+        FLOOR(
+            row_no / 2
+        ) + 1 AS batch_no,
         api_key
     FROM
-        all_contracts
+        base
         JOIN api_keys
         ON 1 = 1
 ),
-batched AS ({% for item in range(101) %}
+batched AS ({% for item in range(150) %}
 SELECT
     rn.contract_address, ethereum.streamline.udf_api('GET', CONCAT('https://api-optimistic.etherscan.io/api?module=contract&action=getabi&address=', rn.contract_address, '&apikey=', api_key),{ 'User-Agent': 'FlipsideStreamline' },{}) AS abi_data, SYSDATE() AS _inserted_timestamp
 FROM
     row_nos rn
 WHERE
-    row_no = {{ item }}
-
-    {% if not loop.last %}
+    batch_no = {{ item }} + 1
+    AND EXISTS (
+SELECT
+    1
+FROM
+    row_nos
+LIMIT
+    1) {% if not loop.last %}
     UNION ALL
     {% endif %}
 {% endfor %})
