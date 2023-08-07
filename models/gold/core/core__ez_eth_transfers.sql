5e60d9e3af37aabc889621f3e658ae4b74ce482e
Austin <austin@flipsidecrypto.com>
2023-08-07 17:46:49 -0400

    table

--- models/gold/core/core__ez_eth_transfers.sql
+++ models/gold/core/core__ez_eth_transfers.sql
@@ -1,5 +1,9 @@
 {{ config(
-    materialized = 'view'
+    materialized = 'incremental',
+    incremental_strategy = 'delete+insert',
+    unique_key = 'block_number',
+    cluster_by = ['block_timestamp::DATE'],
+    tags = ['core','non_realtime']
 ) }}
 
 WITH eth_base AS (
@@ -8,12 +12,12 @@ WITH eth_base AS (
         tx_hash,
         block_number,
         block_timestamp,
+        identifier,
         from_address,
         to_address,
         eth_value,
-        identifier,
         _call_id,
-        input,
+        _inserted_timestamp,
         to_varchar(
             TO_NUMBER(REPLACE(DATA :value :: STRING, '0x'), REPEAT('X', LENGTH(REPLACE(DATA :value :: STRING, '0x'))))
         ) AS eth_value_precise_raw,
@@ -35,38 +39,69 @@ WITH eth_base AS (
         eth_value > 0
         AND tx_status = 'SUCCESS'
         AND trace_status = 'SUCCESS'
-),
-eth_price AS (
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
     SELECT
-        HOUR,
-        price AS eth_price
+        MAX(_inserted_timestamp) - INTERVAL '72 hours'
     FROM
-        {{ ref('silver__prices_eth') }}
+        {{ this }}
+)
+{% endif %}
+),
+tx_table AS (
+    SELECT
+        block_number,
+        tx_hash,
+        from_address AS origin_from_address,
+        to_address AS origin_to_address,
+        origin_function_signature
+    FROM
+        {{ ref('silver__transactions') }}
+    WHERE
+        tx_hash IN (
+            SELECT
+                DISTINCT tx_hash
+            FROM
+                eth_base
+        )
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp) - INTERVAL '72 hours'
+    FROM
+        {{ this }}
+)
+{% endif %}
 )
 SELECT
-    A.tx_hash AS tx_hash,
-    A.block_number AS block_number,
-    A.block_timestamp AS block_timestamp,
-    A.identifier AS identifier,
-    tx.from_address AS origin_from_address,
-    tx.to_address AS origin_to_address,
-    tx.origin_function_signature AS origin_function_signature,
-    A.from_address AS eth_from_address,
-    A.to_address AS eth_to_address,
-    A.eth_value AS amount,
-    A.eth_value_precise AS amount_precise,
-    A.eth_value_precise_raw AS amount_precise_raw,
+    tx_hash AS tx_hash,
+    block_number AS block_number,
+    block_timestamp AS block_timestamp,
+    identifier AS identifier,
+    origin_from_address,
+    origin_to_address,
+    origin_function_signature,
+    from_address AS eth_from_address,
+    to_address AS eth_to_address,
+    eth_value AS amount,
+    eth_value_precise_raw AS amount_precise_raw,
+    eth_value_precise AS amount_precise,
     ROUND(
-        A.eth_value * eth_price,
+        eth_value * price,
         2
-    ) AS amount_usd
+    ) AS amount_usd,
+    _call_id,
+    _inserted_timestamp
 FROM
     eth_base A
-    LEFT JOIN eth_price
+    LEFT JOIN {{ ref('silver__prices_eth') }}
     ON DATE_TRUNC(
         'hour',
-        block_timestamp
+        A.block_timestamp
     ) = HOUR
-    JOIN {{ ref('silver__transactions') }}
-    tx
-    ON A.tx_hash = tx.tx_hash
+    JOIN tx_table USING (
+        tx_hash,
+        block_number
+    )
