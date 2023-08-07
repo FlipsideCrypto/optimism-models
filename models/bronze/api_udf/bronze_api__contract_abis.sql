5804388af6890e3b01b11c8fdba30d3c178770ec
Austin <austin@flipsidecrypto.com>
2023-08-07 11:21:51 -0400

    adjustments

--- models/bronze/api_udf/bronze_api__contract_abis.sql
+++ models/bronze/api_udf/bronze_api__contract_abis.sql
@@ -33,7 +33,49 @@ WHERE
     abi_data :data :result :: STRING <> 'Max rate limit reached'
 {% endif %}
 LIMIT
-    300
+    50
+), all_contracts AS (
+    SELECT
+        contract_address
+    FROM
+        base
+    UNION
+    SELECT
+        contract_address
+    FROM
+        (
+            SELECT
+                contract_address,
+                MAX(block_number) AS block_number,
+                COUNT(*) AS events
+            FROM
+                {{ ref("silver__logs") }}
+                l
+                LEFT JOIN {{ source(
+                    'optimism_silver',
+                    'verified_abis'
+                ) }}
+                -- this has to be a source or else we get a circular dependency
+                v USING (contract_address)
+            WHERE
+                l.block_timestamp >= CURRENT_DATE - INTERVAL '30 days' -- recent activity
+                AND v.contract_address IS NULL -- no verified abi
+                AND l.contract_address NOT IN (
+                    SELECT
+                        contract_address
+                    FROM
+                        {{ this }}
+                    WHERE
+                        _inserted_timestamp >= CURRENT_DATE - INTERVAL '30 days' -- this wont let us retry the same contract within 30 days
+                        AND abi_data :data :result :: STRING <> 'Max rate limit reached' -- exclude contracts that we were rate limited on
+                )
+            GROUP BY
+                contract_address
+            ORDER BY
+                events DESC
+            LIMIT
+                50
+        )
 ), row_nos AS (
     SELECT
         contract_address,
@@ -41,29 +83,21 @@ LIMIT
             ORDER BY
                 contract_address
         ) AS row_no,
-        FLOOR(
-            row_no / 2
-        ) + 1 AS batch_no,
         api_key
     FROM
-        base
+        all_contracts
         JOIN api_keys
         ON 1 = 1
 ),
-batched AS ({% for item in range(150) %}
+batched AS ({% for item in range(101) %}
 SELECT
     rn.contract_address, ethereum.streamline.udf_api('GET', CONCAT('https://api-optimistic.etherscan.io/api?module=contract&action=getabi&address=', rn.contract_address, '&apikey=', api_key),{ 'User-Agent': 'FlipsideStreamline' },{}) AS abi_data, SYSDATE() AS _inserted_timestamp
 FROM
     row_nos rn
 WHERE
-    batch_no = {{ item }} + 1
-    AND EXISTS (
-SELECT
-    1
-FROM
-    row_nos
-LIMIT
-    1) {% if not loop.last %}
+    row_no = {{ item }}
+
+    {% if not loop.last %}
     UNION ALL
     {% endif %}
 {% endfor %})
--- models/gold/core/core__ez_eth_transfers.sql
+++ models/gold/core/core__ez_eth_transfers.sql
@@ -13,7 +13,22 @@ WITH eth_base AS (
         eth_value,
         identifier,
         _call_id,
-        input
+        input,
+        to_varchar(
+            TO_NUMBER(REPLACE(DATA :value :: STRING, '0x'), REPEAT('X', LENGTH(REPLACE(DATA :value :: STRING, '0x'))))
+        ) AS eth_value_precise_raw,
+        IFF(LENGTH(eth_value_precise_raw) > 18, LEFT(eth_value_precise_raw, LENGTH(eth_value_precise_raw) - 18) || '.' || RIGHT(eth_value_precise_raw, 18), '0.' || LPAD(eth_value_precise_raw, 18, '0')) AS rough_conversion,
+        IFF(
+            POSITION(
+                '.000000000000000000' IN rough_conversion
+            ) > 0,
+            LEFT(rough_conversion, LENGTH(rough_conversion) - 19),
+            REGEXP_REPLACE(
+                rough_conversion,
+                '0*$',
+                ''
+            )
+        ) AS eth_value_precise
     FROM
         {{ ref('silver__traces') }}
     WHERE
@@ -39,6 +54,8 @@ SELECT
     A.from_address AS eth_from_address,
     A.to_address AS eth_to_address,
     A.eth_value AS amount,
+    A.eth_value_precise AS amount_precise,
+    A.eth_value_precise_raw AS amount_precise_raw,
     ROUND(
         A.eth_value * eth_price,
         2
