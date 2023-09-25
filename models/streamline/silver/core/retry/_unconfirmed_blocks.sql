f002c90c87431f39d7dbd61bd947f13da50835ea
Austin <austin@flipsidecrypto.com>
2023-09-25 14:25:55 -0400

    typo

--- models/streamline/silver/core/retry/_unconfirmed_blocks.sql
+++ models/streamline/silver/core/retry/_unconfirmed_blocks.sql
@@ -5,27 +5,26 @@
 WITH lookback AS (
 
     SELECT
-        MAX(block_number) AS block_number
+        block_number
     FROM
-        {{ ref("silver__blocks") }}
-    WHERE
-        block_timestamp >= DATEADD('hour', -72, SYSDATE()))
-    SELECT
-        DISTINCT cb.block_number AS block_number
-    FROM
-        {{ ref("silver__confirmed_blocks") }}
-        cb
-        LEFT JOIN {{ ref("silver__transactions") }}
-        txs USING (
-            block_number,
-            block_hash,
-            tx_hash
-        )
-    WHERE
-        txs.tx_hash IS NULL
-        AND cb.block_number >= (
-            SELECT
-                block_number
-            FROM
-                lookback
-        )
+        {{ ref("_block_lookback") }}
+)
+SELECT
+    DISTINCT cb.block_number AS block_number
+FROM
+    {{ ref("silver__confirmed_blocks") }}
+    cb
+    LEFT JOIN {{ ref("silver__transactions") }}
+    txs USING (
+        block_number,
+        block_hash,
+        tx_hash
+    )
+WHERE
+    txs.tx_hash IS NULL
+    AND cb.block_number >= (
+        SELECT
+            block_number
+        FROM
+            lookback
+    )
