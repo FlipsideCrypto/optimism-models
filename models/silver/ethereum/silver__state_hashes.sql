c439380ea9964fd62b6fde7a4cab6668df84cd76
mattromano <mattromano88@gmail.com>
2023-12-19 15:01:37 -0800

    update qualify statement

--- models/silver/ethereum/silver__state_hashes.sql
+++ models/silver/ethereum/silver__state_hashes.sql
@@ -77,4 +77,4 @@ FROM
     ON block_number BETWEEN state_min_block
     AND state_max_block QUALIFY(ROW_NUMBER() over(PARTITION BY block_number
     ORDER BY
-    _inserted_timestamp DESC)) = 1
+    state_block_timestamp DESC)) = 1
