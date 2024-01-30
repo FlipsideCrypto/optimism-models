585c8aa2e9a7dc424099bb359900e80c9e30e596
Austin <93135983+austinFlipside@users.noreply.github.com>
2024-01-30 12:17:29 -0500

    is deleted udpates (#241)
    * is deleted udpates
    
    * fix

--- models/bronze/labels/bronze__labels.sql
+++ models/bronze/labels/bronze__labels.sql
@@ -11,11 +11,14 @@ SELECT
     label_type,
     label_subtype,
     address_name,
-    project_name
+    project_name,
+    _is_deleted,
+    modified_timestamp,
+    labels_combined_id
 FROM
     {{ source(
-        'crosschain',
-        'dim_labels'
+        'silver_crosschain',
+        'labels_combined'
     ) }}
 WHERE
     blockchain = 'optimism'
