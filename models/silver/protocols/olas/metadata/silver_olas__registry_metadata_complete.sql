e266c1259bdf6a42b256f6d3b6d71a3532627bec
drethereum <71602799+drethereum@users.noreply.github.com>
2024-06-25 16:49:40 -0600

    AN-4881/agent-activity (#286)
    * initial models
    
    * docs
    
    * docs and tests
    
    * overview docs
    
    * updates for service reads
    
    * coalesce
    
    * row num
    
    * heal
    
    * inc
    
    * remove test
    
    * rebuild
    
    * remove docs

---
+++ models/silver/protocols/olas/metadata/silver_olas__registry_metadata_complete.sql
@@ -0,0 +1,35 @@
+{{ config(
+    materialized = 'view'
+) }}
+
+SELECT
+    m.name,
+    m.description,
+    m.registry_id,
+    m.contract_address,
+    CASE
+        WHEN m.contract_address = '0x3d77596beb0f130a4415df3d2d8232b3d3d31e44' THEN 'Service'
+    END AS registry_type,
+    m.trait_type,
+    m.trait_value,
+    m.code_uri_link,
+    m.image_link,
+    s.agent_ids,
+    m.registry_metadata_id,
+    m.inserted_timestamp,
+    GREATEST(
+        COALESCE(
+            m.modified_timestamp,
+            '1970-01-01' :: TIMESTAMP
+        ),
+        COALESCE(
+            s.modified_timestamp,
+            '1970-01-01' :: TIMESTAMP
+        )
+    ) AS modified_timestamp
+FROM
+    {{ ref('silver_olas__registry_metadata') }}
+    m
+    LEFT JOIN {{ ref('silver_olas__getservice_reads') }}
+    s
+    ON m.registry_id = s.function_input
