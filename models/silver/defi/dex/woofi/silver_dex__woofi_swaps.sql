f363de03a79a672d74a9863c5d039d205ac7b537
drethereum <71602799+drethereum@users.noreply.github.com>
2024-10-03 12:53:20 -0600

    AN-5304/woofi-v2 (#308)
    * new contracts
    
    * persist docs

--- models/silver/defi/dex/woofi/silver_dex__woofi_swaps.sql
+++ models/silver/defi/dex/woofi/silver_dex__woofi_swaps.sql
@@ -4,7 +4,6 @@
     unique_key = 'block_number',
     cluster_by = ['block_timestamp::DATE'],
     tags = ['curated','reorg']
-
 ) }}
 
 WITH router_swaps_base AS (
@@ -60,7 +59,8 @@ WITH router_swaps_base AS (
         l
     WHERE
         contract_address IN (
-            '0xeaf1ac8e89ea0ae13e0f03634a4ff23502527024' --v2
+            '0xeaf1ac8e89ea0ae13e0f03634a4ff23502527024',
+            '0x4c4af8dbc524681930a27b2f1af5bcc8062e6fb7' --v2
         )
         AND topics [0] :: STRING = '0x27c98e911efdd224f4002f6cd831c3ad0d2759ee176f9ee8466d95826af22a1c' --WooRouterSwap
         AND tx_status = 'SUCCESS'
@@ -120,7 +120,10 @@ swaps_base AS (
         {{ ref('silver__logs') }}
         l
     WHERE
-        contract_address = '0xeaf1ac8e89ea0ae13e0f03634a4ff23502527024' --v2
+        contract_address IN (
+            '0xeaf1ac8e89ea0ae13e0f03634a4ff23502527024',
+            '0xed9e3f98bbed560e66b89aac922e29d4596a9642'
+        ) --v2
         AND topics [0] :: STRING = '0x0e8e403c2d36126272b08c75823e988381d9dc47f2f0a9a080d95f891d95c469' --WooSwap
         AND tx_hash NOT IN (
             SELECT
