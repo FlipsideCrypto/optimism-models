8400e8c934aa4981ea80ab722933f3bf00a9e85f
Austin <austin@flipsidecrypto.com>
2025-01-29 12:32:42 -0500

    tests

--- tests/optimism/test_silver__recent_missing_traces.sql
+++ tests/optimism/test_silver__recent_missing_traces.sql
@@ -1,2 +1,2 @@
 -- depends_on: {{ ref('test_silver__transactions_recent') }}
-{{ recent_missing_txs(ref("test_silver__traces_recent")) }}
+{{ recent_missing_txs(ref("test_gold__fact_traces_recent")) }}
