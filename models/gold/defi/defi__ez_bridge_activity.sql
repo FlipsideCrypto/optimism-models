fd8a190af162e9085cab64935bc4cae828b1a56e
San Yong <22216004+SanYongxie@users.noreply.github.com>
2025-04-15 09:30:40 +0800

    AN-5863/ccip-bridging (#337)
    * initial commit
    
    * updated complete and gold tables

--- models/gold/defi/defi__ez_bridge_activity.sql
+++ models/gold/defi/defi__ez_bridge_activity.sql
@@ -5,7 +5,7 @@
     meta ={ 
         'database_tags':{
             'table':{
-                'PROTOCOL': 'ACROSS, AXELAR, CELER, CBRIDGE, DLN, DEBRIDGE, EYWA, HOP, MESON, MULTICHAIN, STARGATE, SYMBIOSIS, SYNAPSE, WORMHOLE',
+                'PROTOCOL': 'ACROSS, AXELAR, CELER, CBRIDGE, DLN, DEBRIDGE, EYWA, HOP, MESON, MULTICHAIN, STARGATE, SYMBIOSIS, SYNAPSE, WORMHOLE, CCIP',
                 'PURPOSE': 'BRIDGE'
         } } }
 ) }}
