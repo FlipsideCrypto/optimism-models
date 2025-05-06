695d9eaf1d2d38f217741469fd035bbd5de4ea63
drethereum <trevor.wenokur@gmail.com>
2025-05-06 12:37:56 -0600

    docs

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -1,16 +1,7 @@
 {% macro create_udfs() %}
-    {% if var("UPDATE_UDFS_AND_SPS") %}
+    {% if var("UPDATE_UDFS_AND_SPS", false) %}
         {% set sql %}
         CREATE schema if NOT EXISTS silver;
-
-        {{ create_udtf_get_base_table(
-            schema = "streamline"
-        ) }}
-        {{ create_udf_get_chainhead() }}
-        {{ create_udf_bulk_json_rpc() }}
-        {{ create_udf_bulk_decode_logs() }}
-        {{ create_udf_bulk_get_traces() }}
-        
         {% endset %}
         {% do run_query(sql) %}
         {{- fsc_utils.create_udfs() -}}
