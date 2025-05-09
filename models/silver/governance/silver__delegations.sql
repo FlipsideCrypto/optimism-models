3245a0892f70801fbf80e6a8f9a1ccc39ec2400f
Austin <austin@flipsidecrypto.com>
2025-05-09 10:01:43 -0400

    update delegations model

--- models/silver/governance/silver__delegations.sql
+++ models/silver/governance/silver__delegations.sql
@@ -6,141 +6,74 @@
     tags = ['silver','governance', 'curated']
 ) }}
 
-SELECT
-    block_number,
-    block_timestamp,
-    tx_hash,
-    IFF(
-        tx_succeeded,
-        'SUCCESS',
-        'FAIL'
-    ) AS status,
-    CASE
-        WHEN topics [0] :: STRING = '0x3134e8a2e6d97e929a7e54011ea5485d7d196dd5f0ba4d4ef95803e8e3fc257f' THEN 'DelegateChanged'
-        WHEN topics [0] :: STRING = '0xdec2bacdd2f05b59de34da9b523dff8be42e5e38e818c82fdb0bae774387a724' THEN 'DelegateVotesChanged'
-    END AS event_name,
-    origin_from_address AS delegator,
-    CASE
-        WHEN CONCAT(
-            '0x',
-            SUBSTR(
-                topics [2] :: STRING,
-                27,
-                40
+with base as (
+    SELECT 
+        block_number,
+        block_timestamp,
+        tx_hash,
+        iff(tx_succeeded, 'SUCCESS', 'FAIL') AS status,
+        event_name,
+        origin_from_address,
+        origin_to_address,
+        decoded_log,
+        decoded_log:delegate::string as delegate,
+        try_to_number(decoded_log:newBalance::string) as new_balance,
+        try_to_number(decoded_log:previousBalance::string) as previous_balance,
+        decoded_log:delegator::string as delegator,
+        decoded_log:fromDelegate::string as from_delegate,
+        decoded_log:toDelegate::string as to_delegate
+    FROM {{ ref('core__ez_decoded_event_logs') }}
+    WHERE contract_address = '0x4200000000000000000000000000000000000042'
+    AND event_name IN ('DelegateChanged', 'DelegateVotesChanged')
+    AND tx_succeeded
+    {% if is_incremental() %}
+    AND modified_timestamp >= (
+        SELECT
+            MAX(
+                _inserted_timestamp
             )
-        ) = '0x0000000000000000000000000000000000000000'
-        AND CONCAT(
-            '0x',
-            SUBSTR(
-                topics [3] :: STRING,
-                27,
-                40
-            )
-        ) <> delegator THEN 'First Time Delegator'
-        WHEN CONCAT(
-            '0x',
-            SUBSTR(
-                topics [2] :: STRING,
-                27,
-                40
-            )
-        ) = '0x0000000000000000000000000000000000000000'
-        AND delegator = CONCAT(
-            '0x',
-            SUBSTR(
-                topics [3] :: STRING,
-                27,
-                40
-            )
-        ) THEN 'First Time Delegator - Self Delegation'
-        WHEN delegator = CONCAT(
-            '0x',
-            SUBSTR(
-                topics [3] :: STRING,
-                27,
-                40
-            )
-        ) THEN 'Self-Delegation'
-        ELSE 'Re-Delegation'
-    END AS delegation_type,
-    CASE
-        WHEN delegation_type = 'Re-Delegation'
-        AND event_name = 'DelegateVotesChanged' THEN CONCAT('0x', SUBSTR(topics [1] :: STRING, 27, 40))
-        ELSE CONCAT(
-            '0x',
-            SUBSTR(
-                topics [3] :: STRING,
-                27,
-                40
-            )
-        )
-    END AS to_delegate,
-    CASE
-        WHEN delegation_type = 'Re-Delegation' THEN CONCAT(
-            '0x',
-            SUBSTR(
-                topics [2] :: STRING,
-                27,
-                40
-            )
-        )
-        WHEN delegation_type = 'First Time Delegator'
-        AND event_name = 'DelegateChanged' THEN CONCAT(
-            '0x',
-            SUBSTR(
-                topics [2] :: STRING,
-                27,
-                40
-            )
-        )
-        ELSE NULL
-    END AS from_delegate,
-    COALESCE(
-        TRY_TO_NUMBER(
-            utils.udf_hex_to_int(
-                regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') [0] :: STRING)
-            ),
-            0
-        ) AS raw_previous_balance,
-        COALESCE(
-            TRY_TO_NUMBER(
-                utils.udf_hex_to_int(
-                    regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') [1] :: STRING)
-                ),
-                0
-            ) AS raw_new_balance,
-            COALESCE(raw_new_balance / pow(10, 18), 0) AS new_balance,
-            COALESCE(raw_previous_balance / pow(10, 18), 0) AS previous_balance,
-            modified_timestamp AS _inserted_timestamp,
-            CONCAT(
-                tx_hash :: STRING,
-                '-',
-                event_index :: STRING
-            ) AS _log_id,
-            {{ dbt_utils.generate_surrogate_key(
-                ['tx_hash', 'event_index']
-            ) }} AS delegations_id,
-            SYSDATE() AS inserted_timestamp,
-            SYSDATE() AS modified_timestamp,
-            '{{ invocation_id }}' AS _invocation_id
-            FROM
-                {{ ref('core__fact_event_logs') }}
-            WHERE
-                origin_function_signature = '0x5c19a95c'
-                AND origin_to_address = '0x4200000000000000000000000000000000000042'
-                AND topics [0] :: STRING IN (
-                    '0x3134e8a2e6d97e929a7e54011ea5485d7d196dd5f0ba4d4ef95803e8e3fc257f',
-                    '0xdec2bacdd2f05b59de34da9b523dff8be42e5e38e818c82fdb0bae774387a724'
-                )
-                AND to_delegate IS NOT NULL
-
-{% if is_incremental() %}
-AND modified_timestamp >= (
-    SELECT
-        MAX(
-            _inserted_timestamp
-        )
-    FROM
-        {{ this }}
+        FROM {{ this }}
+    )
+    {% endif %}
+), 
+change_info as (
+    select 
+        block_number,
+        tx_hash,
+        delegator,
+        from_delegate,
+        to_delegate,
+        case 
+            when delegator = to_delegate and from_delegate = '0x0000000000000000000000000000000000000000' then 'First Time Delegator - Self Delegation'
+            when delegator = to_delegate and from_delegate <> '0x0000000000000000000000000000000000000000' then 'Self Delegation'
+            when delegator <> to_delegate and from_delegate = '0x0000000000000000000000000000000000000000' then 'First Time Delegator'
+            else 'Re-Delegation'
+        end as delegation_type
+    from base 
+    where event_name = 'DelegateChanged'
 )
-{% endif %}
+SELECT
+    b0.block_number,
+    b0.block_timestamp,
+    b0.tx_hash,
+    b0.status,
+    b0.event_name,
+    b1.delegator,
+    b1.delegation_type,
+    b1.to_delegate,
+    b1.from_delegate,
+    b0.delegate,
+    b0.previous_balance as raw_previous_balance,
+    b0.new_balance as raw_new_balance,
+    COALESCE(raw_new_balance / pow(10, 18), 0) AS new_balance,
+    COALESCE(raw_previous_balance / pow(10, 18), 0) AS previous_balance,
+    modified_timestamp AS _inserted_timestamp,
+    CONCAT(tx_hash :: STRING, '-', event_index :: STRING) AS _log_id,
+    {{ dbt_utils.generate_surrogate_key(['tx_hash', 'event_index']) }} AS delegations_id,
+    SYSDATE() AS inserted_timestamp,
+    SYSDATE() AS modified_timestamp,
+    '{{ invocation_id }}' AS _invocation_id
+    FROM 
+        base b0
+    JOIN change_info b1 using (block_number, tx_hash)
+    WHERE b0.event_name = 'DelegateVotesChanged'
