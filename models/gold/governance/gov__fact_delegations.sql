e95072b9a5a1b86e6dc2fc6c6413f9903ab67b9f
Austin <austin@flipsidecrypto.com>
2025-05-14 10:06:05 -0400

    docs and gold

--- models/gold/governance/gov__fact_delegations.sql
+++ models/gold/governance/gov__fact_delegations.sql
@@ -12,26 +12,18 @@ SELECT
     status, 
     event_name,
     delegator, 
-    to_delegate, 
-    from_delegate, 
+    delegate,
     delegation_type,
     new_balance,
     previous_balance,
     raw_new_balance, 
-    raw_previous_balance,	    
-    COALESCE (
-        delegations_id,
-        {{ dbt_utils.generate_surrogate_key(
-            ['_log_id']
-        ) }}
-    ) AS fact_delegations_id,
-    COALESCE(
-        inserted_timestamp,
-        '2000-01-01'
-    ) AS inserted_timestamp,
-    COALESCE(
-        modified_timestamp,
-        '2000-01-01'
-    ) AS modified_timestamp
+    raw_previous_balance,	
+    to_delegate, 
+    from_delegate,     
+    {{ dbt_utils.generate_surrogate_key(
+        ['_log_id']
+    ) }} AS fact_delegations_id,
+    inserted_timestamp,
+    modified_timestamp
 FROM 
     {{ ref('silver__delegations') }}
