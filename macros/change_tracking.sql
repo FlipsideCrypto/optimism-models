a51976aab2dd586438bbe0d5cf05aa9c39db7cd8
drethereum <trevor.wenokur@gmail.com>
2025-07-28 14:39:30 -0600

    package

--- macros/change_tracking.sql
+++
@@ -1,9 +0,0 @@
-{% macro enable_change_tracking() %}
-  {% if 'exclude_change_tracking' not in config.get('tags') and var('ENABLE_CHANGE_TRACKING', false) %}
-    {% if config.get('materialized') == 'view' %}
-      ALTER VIEW {{ this }} SET CHANGE_TRACKING = TRUE;
-    {% else %}
-      ALTER TABLE {{ this }} SET CHANGE_TRACKING = TRUE;
-    {% endif %}
-  {% endif %}
-{% endmacro %}
